992301b2cf63676fecd7b76e1f7e4dc53054f873
Konstantinos Mastrapas <mastrabus@gmail.com>
2025-01-17 13:49:57 +0200

    Complete!

--- logic.js
+++ logic.js
@@ -11,13 +11,13 @@ function getComputerChoice() {
 
     switch (randomChoice) {
         case 0:
-            computerChoice = "Rock"
+            computerChoice = "rock"
             break;
         case 1:
-            computerChoice = "Paper"
+            computerChoice = "paper"
             break;
         case 2:
-            computerChoice = "Scissors"
+            computerChoice = "scissors"
             break;
     }
 
@@ -37,36 +37,86 @@ function getHumanChoice() {
     }
 
     //console.log(humanChoice)
-    return humanChoice;
+    return humanChoice.toLowerCase();
 }
 
-let humanScore = 0
-let computerScore = 0
-
 function playRound(humanChoice, computerChoice) {
+    
     switch (humanChoice) {
         case "rock":
             if (computerChoice == "rock") {
                 console.log("Draw.")
+                return 0;
             } else if (computerChoice == "paper") {
                 console.log("Lose. Paper beats Rock.")
-            } else (computerChoice == "scissors"); {
+                return -1;
+            } else if (computerChoice == "scissors") {
                 console.log("Win. Rock beats Scissors.")
+                return 1;
             }
             break;
         case "paper":
             if (computerChoice == "paper") {
                 console.log("Draw.")
+                return 0;
             } else if (computerChoice == "scissors") {
                 console.log("Lose. Scissors beats Paper.")
-            } else (computerChoice == "rock"); {
+                return -1;
+            } else if (computerChoice == "rock") {
                 console.log("Win. Paper beats Rock.")
+                return 1;
+            }
+            break;
+        case "scissors":
+            if (computerChoice == "scissors") {
+                console.log("Draw.")
+                return 0;
+            } else if (computerChoice == "rock") {
+                console.log("Lose. Rock beats Scissors.")
+                return -1;
+            } else if (computerChoice == "paper") {
+                console.log("Win. Scissors beats Paper.")
+                return 1;
             }
             break;
     }
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
+function playGame() {
+    const rounds = 6 // Its 5 but set to 6 to compansate for starting i from 1
+    let currentround = 0
+    let humanScore = 0
+    let computerScore = 0
 
-playRound(humanSelection, computerSelection);
+    for(i = 1; i < rounds; i++) {
+        console.log("Starting Round " + i)
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+
+        let winner = playRound(humanSelection, computerSelection);
+
+        if (winner == 1) {
+            humanScore++
+            currentround++
+        }
+        else if (winner == -1) {
+            computerScore++
+            currentround++
+        }
+        else {
+            currentround++
+        }
+
+        if (currentround == 5) {
+            console.log("FINAL! Human: " + humanScore + " Computer: " + computerScore)
+        }
+        else {
+            console.log("Human: " + humanScore + " Computer: " + computerScore)
+        }
+    }
+}
+
+playGame()
+
+
+
